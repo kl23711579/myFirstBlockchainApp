47e27b2134ff5beab2da50c93b7c3ab413ad1710
kl23711579 <kl23711579@gmail.com>
2021-10-13 23:50:19 +1000

    render task list

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createdTask("Hello World!");
+    }
+
+    function createdTask(string memory _content) public {
+        // determine the id of task
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
 
